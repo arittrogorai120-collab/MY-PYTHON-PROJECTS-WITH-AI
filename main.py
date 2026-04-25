81104348bd59d2f0a986c1a61bc888e7be008ee6
arittrogorai120-collab <arittrogorai120@gmail.com>
2026-04-25 02:13:46 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+n = int(input("enter a no"))
+
+# upper half
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+
+    for j in range(1, i + 1):
+        print(j, end="")
+
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+
+    print()
+
+# lower half
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i), end="")
+
+    for j in range(1, i + 1):
+        print(j, end="")
+
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+
+    print()
